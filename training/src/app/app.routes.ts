dc399c61a040f637d932867be336dace62c53bd0
Pranav Naik <pranav.naik.-nd@disney.com>
2025-06-16 17:20:55 +0530

    reactive forms

--- training/src/app/app.routes.ts
+++ training/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TemplateDrivenForm } from './template-driven-form/template-driven-form';
 import { UserList } from './user-list/user-list';
+import { ReactiveForm } from './reactive-form/reactive-form';
 
 export const routes: Routes = [
   {path: 'template-driven-form', component: TemplateDrivenForm},
-  {path: 'user-list', component: UserList}
+  {path: 'user-list', component: UserList},
+  {path: 'rf', component: ReactiveForm}
 ];
